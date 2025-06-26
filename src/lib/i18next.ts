7a2f0e17069ebc40cba8baf15246a43ca963a633
Daniel Martín <dmd.27.3.1994@.com>
2025-06-26 22:23:45 +0200

    fix: i18n

--- src/lib/i18next.ts
+++ src/lib/i18next.ts
@@ -1,12 +1,17 @@
 import i18next from 'i18next';
-import * as es from '../translations/es.json';
-import * as en from '../translations/en.json';
+import es from '../translations/es.json';
+import en from '../translations/en.json';
 
 i18next.init({
   fallbackLng: 'es',
-  ns: ['en', 'es'],
-  defaultNS: 'es',
+  defaultNS: 'translation',
   debug: true,
+  resources: {
+    en: {
+      translation: en,
+    },
+    es: {
+      translation: es,
+    },
+  },
 });
-i18next.addResourceBundle('en', 'en', en, true, true);
-i18next.addResourceBundle('es', 'es', es, true, true);
